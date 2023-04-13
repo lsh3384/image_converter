cb7e5929f5474e8a7248790a8897e7268b254ed9
lsh3384 <lsh3384@gmail.com>
2023-04-14 08:05:36 +0900

    image preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [preview, setPreview] = useState<File>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+    setPreview(event.target.files[0]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <img
+          style={{ width: "300px", objectFit: "cover", borderRadius: "4px" }}
+          src={preview === undefined ? "" : URL.createObjectURL(preview)}
+          alt=""
+        />
+      </div>
+      <form>
+        <input type="file" id="file" onChange={handleChange} />
+        <label htmlFor="file">
+          <span>upload image</span>
+          <img src="https://img.icons8.com/parakeet/2x/add-image.png" alt="" />
+        </label>
+      </form>
     </div>
   );
 }
